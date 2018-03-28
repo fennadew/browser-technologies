a0cc6b0c30b9f3e75e6b360661cf8dd886e1fc86
Fenna <fennadewilde@hotmail.com>
2018-03-28 22:37:10 +0200

    enriched with js

---
+++ opdracht3/public/javascripts/main.js
@@ -0,0 +1,45 @@
+(function () {
+        'use strict';
+        var app = {
+            init: function () {
+                if (document.addEventListener) {
+                    var form = document.getElementById('form-contacts');
+                    var input = document.getElementById('input-name');
+                    form.setAttribute('class', 'enriched');
+                    input.addEventListener('keyup', function (e) {
+                        filterContacts(this.value);
+                    });
+                }
+            }
+        };
+
+        function filterContacts(input) {
+            var list = document.getElementById("contacts");
+            var names = list.getElementsByTagName("a");
+            var inputNew = input.toLowerCase();
+
+            for (var i = 0; i < names.length; i++) {
+                var name = names[i].innerText.toLowerCase();
+                if (!String.prototype.startsWith) {
+                    if (name.innerHTML.toLowerCase().indexOf(inputNew) > -1) {
+                        articles[i].style.display = "";
+                    } else if (name.innerHTML.toLowerCase().indexOf(inputNew) > -1) {
+                        names[i].parentNode.setAttribute('class', '')
+                    } else {
+                        names[i].parentNode.setAttribute('class', 'hidden')
+                    }
+                } else {
+                    if (name.startsWith(inputNew)) {
+                        names[i].parentNode.setAttribute('class', '')
+                    } else {
+                        names[i].parentNode.setAttribute('class', 'hidden')
+                    }
+                }
+            }
+        }
+
+
+        app.init();
+    }
+
+)();
